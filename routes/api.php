bdff5f7797b1ce2e0d0aafe29cc395a86ec9792c
Frank Sierra <sierrafayad@gmail.com>
2019-04-17 10:19:00 -0500

    Routes for api calls

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->namespace('v1')->middleware('api.authorization')->group(function () {
-    Route::prefix('emails')->namespace('Emails')->group(function () {
+    Route::prefix('emails')->group(function () {
         Route::post('', 'EmailsController@queue')->name('api.v1.emails.queue');
-//        Route::get('','EmailController@index')->name('api.v1.emails.list');
-//        Route::get("{id}", "ApiBranchesController@show");
+    });
+    Route::prefix('messages')->group(function () {
+        Route::post('', 'SMSController@queue')->name('api.v1.messages.queue');
+    });
+    Route::prefix('notifications')->group(function () {
+        Route::post('', 'PushController@queue')->name('api.v1.notifications.queue');
     });
 });
 
