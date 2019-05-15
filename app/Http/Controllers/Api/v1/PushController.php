08682e34af306c475f6127cea89d6ee9763455a4
Frank Sierra <sierrafayad@gmail.com>
2019-05-15 02:33:42 -0500

    Method to get the status and to send both ways sync and async
    Also i added a method to register the device

--- app/Http/Controllers/Api/v1/PushController.php
+++ app/Http/Controllers/Api/v1/PushController.php
@@ -3,24 +3,48 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Jobs\SendPushJob;
+use App\Models\PushDevice;
 use App\Models\PushLog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Support;
 
 class PushController extends Controller
 {
-    public function queue(Request $request)
+
+    public function status($uuid, Request $request)
+    {
+        $app_id = $request->request_log->app_id;
+        $push = PushLog::whereAppId($app_id)->whereUuid($uuid)->first();
+        if ($push) {
+            return response()->json([
+                'push_uuid' => $uuid,
+                'status' => $push->status,
+                'data' => json_decode($push->data)
+            ]);
+        } else {
+            return response()->json(
+                [
+                    'status' => 'missing',
+                    'push_uuid' => $uuid
+                ],
+                404
+            );
+        }
+    }
+
+    private function proccess(Request $request)
     {
         $this->validate($request, [
             'to' => 'required|string',
-            'data' => 'required|string',
-            'notification' => 'nullable|string'
+            'data' => 'required|json',
+            'notification' => 'nullable|json'
         ]);
         $json_request = $request->all();
         $json_request["to"] = json_decode($json_request["to"] ?? "[]");
+        $json_request["data"] = json_decode($json_request["data"] ?? "[]");
+        $json_request["notification"] = json_decode($json_request["notification"] ?? $request->get("data"));
         $request->merge($json_request);
         $this->validate(
             $request,
@@ -50,15 +74,78 @@ class PushController extends Controller
             'status' => 'queued',
             'data' => json_encode([])
         ]);
+        return $details;
+    }
 
-        PushLog::create([
-            'uuid' => $details['uuid'],
+    public function queue(Request $request)
+    {
+        $details = $this->proccess($request);
+        dispatch(new SendPushJob($details));
+        return response()->json([
+            'push_uuid' => $details['uuid'],
             'status' => 'queued',
             'data' => json_encode([])
         ]);
-        dispatch_now(new SendPushJob($details));
+    }
+
+    public function now(Request $request)
+    {
+        $details = $this->proccess($request);
+        $dispatch = dispatch_now(new SendPushJob($details));
         return response()->json([
-            'push_uuid' => $details['uuid']
+            'push_uuid' => $details['uuid'],
+            'status' => $dispatch['status'],
+            'data' => json_decode($dispatch['data'])
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'platform' => 'required|string',
+            'identity' => 'required|string',
+            'regid' => 'required|string'
+        ]);
+        $app_id = $request->request_log->app_id;
+        $platform = $request->get('platform');
+        $identity = $request->get('identity');
+        $uuid = "UID:" . sha1($app_id . $platform . $identity);
+        $regid = $request->get('regid');
+
+        if (!PushDevice::whereUid($uuid)->exists()) {
+            $device = new PushDevice([
+                'app_id' => $app_id,
+                'platform' => $platform,
+                'uuid' => $uuid,
+                'identity' => $identity,
+                'regid' => $regid
+            ]);
+            $device->save();
+            return response()->json([
+                'device_uuid' => $device->uuid
+            ]);
+
+        }
+
+        $device = PushDevice::whereUid($uuid)->first();
+        if ($device->regid == $regid) {
+            return response()->json([
+                'device_uuid' => $device->uuid
+            ]);
+        }
+//        TODO: Create this Log Table
+//        (new PushDeviceLog)->create([
+//            'device_id' => $device->id,
+//            'old_regid' => $device->regid,
+//            'new_regid' => $regid,
+//
+//        ])
+        $device->regid = $regid;
+
+        $device->save();
+
+        return response()->json([
+            'device_uuid' => $device->uuid
         ]);
     }
 }
