1f92dc69084527ad947844db5472ecf169b464e2
Frank Sierra <sierrafayad@gmail.com>
2019-05-15 02:35:54 -0500

    new routes for everything

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->namespace('v1')->middleware('api.authorization')->group(function () {
     Route::prefix('emails')->group(function () {
         Route::post('', 'EmailsController@queue')->name('api.v1.emails.queue');
+        Route::post('/now', 'EmailsController@now')->name('api.v1.emails.now');
+        Route::get('/{uuid}', 'EmailsController@status')->name('api.v1.emails.status');
+    });
+    Route::prefix('push')->group(function () {
+        Route::post('/device', 'PushController@register')->name('api.v1.push.register');
+        Route::post('', 'PushController@queue')->name('api.v1.push.queue');
+        Route::post('/now', 'PushController@now')->name('api.v1.push.now');
+        Route::get('/{uuid}', 'PushController@status')->name('api.v1.push.status');
     });
     Route::prefix('sms')->group(function () {
         Route::post('', 'SMSController@queue')->name('api.v1.sms.queue');
-    });
-    Route::prefix('push')->group(function () {
-        Route::post('', 'PushController@queue')->name('api.v1.push.queue');
+        Route::post('/now', 'SMSController@now')->name('api.v1.sms.now');
+        Route::get('/{uuid}', 'SMSController@status')->name('api.v1.sms.status');
     });
 });
 
