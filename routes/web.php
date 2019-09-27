20cafd91dae18fa157aa468e334d8e8b0dcfcccc
Frank Sierra <sierrafayad@gmail.com>
2019-09-27 10:36:18 -0500

    Elimine el Closure de las rutas para ejecutar route:cache

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['register' => false]);
 
+Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index')->name('home');
