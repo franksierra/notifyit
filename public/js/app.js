03c5ad440d3af26033f70aeddb89bec8b6ec8a42
Frank Sierra <sierrafayad@gmail.com>
2020-01-29 16:05:41 -0500

    Telescope assets

--- public/js/app.js
+++ public/js/app.js
@@ -37048,7 +37048,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     broadcaster: 'pusher',
 //     key: process.env.MIX_PUSHER_APP_KEY,
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     encrypted: true
+//     forceTLS: true
 // });
 
 /***/ }),
@@ -37071,8 +37071,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\devilbox\data\www\notifyit_OLD\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\devilbox\data\www\notifyit_OLD\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\devilbox\data\www\notifyit\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\devilbox\data\www\notifyit\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
